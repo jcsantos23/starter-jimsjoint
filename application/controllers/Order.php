8928b0f9fbd3508777eeed1fe3b7b47aaa6fcabb
jcsantos23 <jcsantos23@gmail.com>
2016-02-17 13:11:08 -0800

    Fixed commit() in Order.php controller

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -106,7 +106,15 @@ class Order extends Application {
 
     // proceed with checkout
     function commit($order_num) {
-        //FIXME
+        //Handle Completion
+        if (!$this->orders->validate($order_num)) {
+            redirect('/order/display_menu/' . $order_num);
+        }
+        $record = $this->orders->get($order_num);
+        $record->date = date(DATE_ATOM);
+        $record->status = 'c';
+        $record->total = $this->orders->total($order_num);
+        $this->orders->update($record);
         redirect('/');
     }
 
